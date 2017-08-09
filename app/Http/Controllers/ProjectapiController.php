0eb69e862982fdbd2fd1ab04e1d8ac3cedb19d4d
Moavaz <ezioauditore144@gmail.com>
2017-08-10 02:20:06 +0500

    created table in initial commit

---
+++ app/Http/Controllers/ProjectapiController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\projectapi;
+use Illuminate\Http\Request;
+
+class ProjectapiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\projectapi  $projectapi
+     * @return \Illuminate\Http\Response
+     */
+    public function show(projectapi $projectapi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\projectapi  $projectapi
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(projectapi $projectapi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\projectapi  $projectapi
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, projectapi $projectapi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\projectapi  $projectapi
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(projectapi $projectapi)
+    {
+        //
+    }
+}
