f6b13b5e0d967063e6ad97c44bcdc996b50943c8
Moavaz <ezioauditore144@gmail.com>
2017-08-12 02:31:45 +0500

    created model fillable to protect from massexception

--- app/projectapi.php
+++ app/projectapi.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class projectapi extends Model
 {
     //
+    protected $fillable = [
+        'firstname',
+        'lastname',
+        'email',
+        'studentid',
+        'image',
+        'contactno'
+    ];
 }
